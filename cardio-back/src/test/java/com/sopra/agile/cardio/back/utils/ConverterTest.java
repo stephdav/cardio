c55dcff8faa2d734f5344ced06bdf1eece1ea55e
stephdav <stephane.david@soprasteria.com>
2016-12-15 10:28:19 +0100

    add some tests

---
+++ cardio-back/src/test/java/com/sopra/agile/cardio/back/utils/ConverterTest.java
@@ -0,0 +1,47 @@
+package com.sopra.agile.cardio.back.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.sopra.agile.cardio.back.model.DbSprint;
+import com.sopra.agile.cardio.back.utils.converter.Converter;
+import com.sopra.agile.cardio.common.model.Sprint;
+
+public class ConverterTest {
+
+    // class to test
+    private Converter converter;
+
+    @Before
+    public void setUp() {
+        converter = new Converter();
+    }
+
+    @Test
+    public void testConvertFromSprintToDbSprint() {
+        Sprint input = new Sprint("id", null, "2016-07-14", null);
+        DbSprint output = converter.map(input);
+        assertNotNull(output);
+        assertEquals("id", output.getId());
+        assertNull(output.getName());
+        assertEquals(new LocalDate(2016, 07, 14), output.getStartDate());
+        assertNull(output.getEndDate());
+    }
+
+    @Test
+    public void testConvertFromDbSprintToSprint() {
+        DbSprint input = new DbSprint("id", null, new LocalDate(2016, 07, 14), null);
+        Sprint output = converter.map(input);
+        assertNotNull(output);
+        assertEquals("id", output.getId());
+        assertNull(output.getName());
+        assertEquals("2016-07-14", output.getStartDate());
+        assertNull(output.getEndDate());
+    }
+
+}
