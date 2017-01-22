7b750a0efc985f184143f05c6930b60dd7ca233d
stephdav <stephane.david@soprasteria.com>
2017-01-22 15:31:34 +0100

    Cucumber tests

--- cardio-integration/src/test/java/com/sopra/agile/cardio/integration/TestRunner.java
+++ cardio-integration/src/test/java/com/sopra/agile/cardio/integration/TestRunner.java
@@ -7,8 +7,8 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:com/sopra/agile/cardio/integration/feature", plugin = { "pretty",
-        "html:target/cucumber", "json:target/cucumber.json" })
+@CucumberOptions(features = "classpath:features", plugin = { "pretty", "html:target/cucumber",
+        "json:target/cucumber.json" })
 @NotThreadSafe
 public class TestRunner {
 
