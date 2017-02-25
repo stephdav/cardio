54eacc32a735d7e4486b02427d02929ba57488fe
stephdav <stephane.david@soprasteria.com>
2017-02-25 10:40:37 +0100

    Stories in Kanban

--- cardio-ui/src/main/resources/public/js/kanban.js
+++ cardio-ui/src/main/resources/public/js/kanban.js
@@ -7,7 +7,22 @@ function initKanban() {
 }
 
 function updateBurnup(data, hv) {
-	$('#story-to-do').text(data.todo.length);
-	$('#story-pending').text(data.pending.length);
-	$('#story-done').text(data.done.length);
+	updateCategory(data.todo, '#stories-ready', '#story-to-do');
+	updateCategory(data.pending, '#stories-pending', '#story-pending');
+	updateCategory(data.done, '#stories-done', '#story-done');
 }
+
+function updateCategory(stories, listSelector, countSelector) {
+	$(countSelector).text(stories.length);
+
+	$(listSelector).empty();
+	content = '';
+	$.each(stories, function(index, story) {
+		content += '<a href="#" class="list-group-item">';
+		content += '<h4 class="list-group-item-heading">#' + story.id + '</h4>';
+		content += '<p class="list-group-item-text">' + story.description + '</p>';
+		content += '</a>';
+	});
+	$(listSelector).append(content);
+}
+
