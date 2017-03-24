a6de1ecc6bb0e332eea193a7dfbf863cdcf77009
Stephane DAVID <sdavid.ext@orange.com>
2017-03-24 14:23:44 +0100

    Fix overlapping

--- cardio-common/src/main/java/com/sopra/agile/cardio/common/model/Sprint.java
+++ cardio-common/src/main/java/com/sopra/agile/cardio/common/model/Sprint.java
@@ -2,131 +2,88 @@ package com.sopra.agile.cardio.common.model;
 
 public class Sprint extends Base {
 
-    private String name;
-    private String startDate;
-    private String endDate;
-    private String goal;
-    private int commitment;
-    private int velocity;
+	private String name;
+	private String startDate;
+	private String endDate;
+	private String goal;
+	private int commitment;
+	private int velocity;
 
-    public Sprint() {
-        super();
-    }
+	public Sprint() {
+		super();
+	}
 
-    public Sprint(final String id, final String name, final String startDate, String endDate) {
-        super(id);
-        this.name = name;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
+	public Sprint(final String id, final String name, final String startDate, String endDate) {
+		super(id);
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = super.hashCode();
-        result = prime * result + commitment;
-        result = prime * result + ((endDate == null) ? 0 : endDate.hashCode());
-        result = prime * result + ((goal == null) ? 0 : goal.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
-        result = prime * result + velocity;
-        return result;
-    }
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (!super.equals(obj)) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Sprint other = (Sprint) obj;
-        if (commitment != other.commitment) {
-            return false;
-        }
-        if (endDate == null) {
-            if (other.endDate != null) {
-                return false;
-            }
-        } else if (!endDate.equals(other.endDate)) {
-            return false;
-        }
-        if (goal == null) {
-            if (other.goal != null) {
-                return false;
-            }
-        } else if (!goal.equals(other.goal)) {
-            return false;
-        }
-        if (name == null) {
-            if (other.name != null) {
-                return false;
-            }
-        } else if (!name.equals(other.name)) {
-            return false;
-        }
-        if (startDate == null) {
-            if (other.startDate != null) {
-                return false;
-            }
-        } else if (!startDate.equals(other.startDate)) {
-            return false;
-        }
-        if (velocity != other.velocity) {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!super.equals(obj)) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		return true;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getStartDate() {
-        return startDate;
-    }
+	public String getStartDate() {
+		return startDate;
+	}
 
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
 
-    public String getEndDate() {
-        return endDate;
-    }
+	public String getEndDate() {
+		return endDate;
+	}
 
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
 
-    public String getGoal() {
-        return goal;
-    }
+	public String getGoal() {
+		return goal;
+	}
 
-    public void setGoal(String goal) {
-        this.goal = goal;
-    }
+	public void setGoal(String goal) {
+		this.goal = goal;
+	}
 
-    public int getCommitment() {
-        return commitment;
-    }
+	public int getCommitment() {
+		return commitment;
+	}
 
-    public void setCommitment(int commitment) {
-        this.commitment = commitment;
-    }
+	public void setCommitment(int commitment) {
+		this.commitment = commitment;
+	}
 
-    public int getVelocity() {
-        return velocity;
-    }
+	public int getVelocity() {
+		return velocity;
+	}
 
-    public void setVelocity(int velocity) {
-        this.velocity = velocity;
-    }
+	public void setVelocity(int velocity) {
+		this.velocity = velocity;
+	}
 }
