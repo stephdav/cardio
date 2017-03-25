2ec9fbc57183ca498cf11f9ff80cb6b9156f231f
stephdav <stephane.david@soprasteria.com>
2017-03-25 14:52:09 +0100

    Remove activities

--- cardio-common/src/main/java/com/sopra/agile/cardio/common/model/ActivityStatus.java
+++
@@ -1,7 +0,0 @@
-package com.sopra.agile.cardio.common.model;
-
-public enum ActivityStatus {
-
-    DRAFT, READY, PENDING, DONE;
-
-}
--- cardio-common/src/main/java/com/sopra/agile/cardio/common/model/ActivityType.java
+++
@@ -1,7 +0,0 @@
-package com.sopra.agile.cardio.common.model;
-
-public enum ActivityType {
-
-    US, TASK;
-
-}
