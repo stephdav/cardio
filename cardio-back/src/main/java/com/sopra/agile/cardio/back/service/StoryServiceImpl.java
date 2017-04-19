fb38b59e097edbfe98e9d9c27cbb73261a2ab76e
Stephane DAVID <sdavid.ext@orange.com>
2017-04-19 09:10:56 +0200

    Fix export of stories

--- cardio-back/src/main/java/com/sopra/agile/cardio/back/service/StoryServiceImpl.java
+++ cardio-back/src/main/java/com/sopra/agile/cardio/back/service/StoryServiceImpl.java
@@ -2,6 +2,9 @@ package com.sopra.agile.cardio.back.service;
 
 import java.util.List;
 
+import org.joda.time.LocalDateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,165 +21,173 @@ import com.sopra.agile.cardio.common.model.StoryStatus;
 @Service
 public class StoryServiceImpl implements StoryService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(StoryServiceImpl.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(StoryServiceImpl.class);
 
-    private static final String SQL_EXPORT = "INSERT INTO STORIES(DESCRIPTION,STATUS,LAST_UPDATE,CONTRIBUTION,ESTIMATE,ASSIGNED) VALUES ('%s', '%s', DATE '%s', %d, %d, %d);\n";
-    private static final String SQL_EXPORT_WITHOUT_USER = "INSERT INTO STORIES(DESCRIPTION,STATUS,LAST_UPDATE,CONTRIBUTION,ESTIMATE) VALUES ('%s', '%s', DATE '%s', %d, %d);\n";
+	private static DateTimeFormatter DATE_EXPORT_FORMAT = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
 
-    @Autowired
-    private StoryDao storyDao;
+	private static final String SQL_EXPORT = "INSERT INTO STORIES(DESCRIPTION,STATUS,LAST_UPDATE,CONTRIBUTION,ESTIMATE,ASSIGNED) VALUES ('%s', '%s', TIMESTAMP '%s', %d, %d, %d);\n";
+	private static final String SQL_EXPORT_WITHOUT_USER = "INSERT INTO STORIES(DESCRIPTION,STATUS,LAST_UPDATE,CONTRIBUTION,ESTIMATE) VALUES ('%s', '%s', TIMESTAMP '%s', %d, %d);\n";
 
-    public StoryServiceImpl() {
-        // Empty constructor
-    }
+	@Autowired
+	private StoryDao storyDao;
 
-    @Override
-    public Parameter count() throws CardioTechnicalException {
-        LOGGER.debug("[SVC] count ...");
-        Parameter param = storyDao.count();
-        if (param == null) {
-            param = new Parameter("STORIES", "0");
-        }
-        return param;
-    }
+	public StoryServiceImpl() {
+		// Empty constructor
+	}
 
-    @Override
-    public List<Parameter> countByStatus() throws CardioTechnicalException {
-        LOGGER.debug("[SVC] count by status ...");
-        return storyDao.countByStatus();
-    }
+	@Override
+	public Parameter count() throws CardioTechnicalException {
+		LOGGER.debug("[SVC] count ...");
+		Parameter param = storyDao.count();
+		if (param == null) {
+			param = new Parameter("STORIES", "0");
+		}
+		return param;
+	}
 
-    @Override
-    public List<Story> all() throws CardioTechnicalException {
-        LOGGER.debug("[SVC] all ...");
-        return storyDao.all();
-    }
+	@Override
+	public List<Parameter> countByStatus() throws CardioTechnicalException {
+		LOGGER.debug("[SVC] count by status ...");
+		return storyDao.countByStatus();
+	}
 
-    @Override
-    public Story find(String id) throws CardioTechnicalException {
-        LOGGER.debug("[SVC] find story '{}' ...", id);
-        Story response = null;
-        try {
-            response = find(Long.parseLong(id));
-        } catch (NumberFormatException e) {
-            throw new CardioTechnicalException("Bad story identifier", e);
-        }
-        return response;
-    }
+	@Override
+	public List<Story> all() throws CardioTechnicalException {
+		LOGGER.debug("[SVC] all ...");
+		return storyDao.all();
+	}
 
-    private Story find(long id) throws CardioTechnicalException {
-        return storyDao.find(id);
-    }
+	@Override
+	public Story find(String id) throws CardioTechnicalException {
+		LOGGER.debug("[SVC] find story '{}' ...", id);
+		Story response = null;
+		try {
+			response = find(Long.parseLong(id));
+		} catch (NumberFormatException e) {
+			throw new CardioTechnicalException("Bad story identifier", e);
+		}
+		return response;
+	}
 
-    @Override
-    public Story add(Story story) throws CardioTechnicalException, CardioFunctionalException {
-        LOGGER.debug("[SVC] add story ...");
-        checkStoryProperties(story);
-        return storyDao.add(story);
-    }
+	private Story find(long id) throws CardioTechnicalException {
+		return storyDao.find(id);
+	}
 
-    @Override
-    public Story update(Story story) throws CardioTechnicalException, CardioFunctionalException {
-        LOGGER.debug("[SVC] update story ...");
-        return update(story, true);
-    }
+	@Override
+	public Story add(Story story) throws CardioTechnicalException, CardioFunctionalException {
+		LOGGER.debug("[SVC] add story ...");
+		checkStoryProperties(story);
+		return storyDao.add(story);
+	}
 
-    @Override
-    public Story patch(Story story) throws CardioTechnicalException, CardioFunctionalException {
-        LOGGER.debug("[SVC] patch story ...");
+	@Override
+	public Story update(Story story) throws CardioTechnicalException, CardioFunctionalException {
+		LOGGER.debug("[SVC] update story ...");
+		return update(story, true);
+	}
 
-        boolean updateTimestamp = false;
+	@Override
+	public Story patch(Story story) throws CardioTechnicalException, CardioFunctionalException {
+		LOGGER.debug("[SVC] patch story ...");
 
-        Story original = find(story.getId());
-        if (story.getDescription() == null) {
-            story.setDescription(original.getDescription());
-        }
-        if (story.getStatus() == null) {
-            story.setStatus(original.getStatus());
-        } else {
-            if (!story.getStatus().equals(original.getStatus())) {
-                updateTimestamp = true;
-            }
-        }
-        if (story.getContribution() == -1) {
-            story.setContribution(original.getContribution());
-        }
-        if (story.getEstimate() == -1) {
-            story.setEstimate(original.getEstimate());
-        }
+		boolean updateTimestamp = false;
 
-        return update(story, updateTimestamp);
-    }
+		Story original = find(story.getId());
+		if (story.getDescription() == null) {
+			story.setDescription(original.getDescription());
+		}
+		if (story.getStatus() == null) {
+			story.setStatus(original.getStatus());
+		} else {
+			if (!story.getStatus().equals(original.getStatus())) {
+				updateTimestamp = true;
+			}
+		}
+		if (story.getContribution() == -1) {
+			story.setContribution(original.getContribution());
+		}
+		if (story.getEstimate() == -1) {
+			story.setEstimate(original.getEstimate());
+		}
 
-    private Story update(Story story, boolean updateTimestamp)
-            throws CardioTechnicalException, CardioFunctionalException {
-        checkStoryProperties(story);
-        return storyDao.update(story, updateTimestamp);
-    }
+		return update(story, updateTimestamp);
+	}
 
-    @Override
-    public Story patch(String id, String status, String contribution, String estimate, String assignedUser)
-            throws CardioTechnicalException, CardioFunctionalException {
-        LOGGER.debug("[SVC] patch story ...");
+	private Story update(Story story, boolean updateTimestamp) throws CardioTechnicalException, CardioFunctionalException {
+		checkStoryProperties(story);
+		return storyDao.update(story, updateTimestamp);
+	}
 
-        Story story = find(id);
-        boolean updateTimestamp = false;
+	@Override
+	public Story patch(String id, String status, String contribution, String estimate, String assignedUser)
+			throws CardioTechnicalException, CardioFunctionalException {
+		LOGGER.debug("[SVC] patch story ...");
 
-        if (status != null) {
-            if (!story.getStatus().toString().equals(status)) {
-                updateTimestamp = true;
-            }
-            story.setStatus(StoryStatus.valueOf(status));
-        }
-        if (contribution != null) {
-            story.setContribution(Integer.parseInt(contribution));
-        }
-        if (estimate != null) {
-            story.setEstimate(Integer.parseInt(estimate));
-        }
-        if (assignedUser != null) {
-            story.setAssignedUser(Long.parseLong(assignedUser));
-        }
-        return update(story, updateTimestamp);
-    }
+		Story story = find(id);
+		boolean updateTimestamp = false;
 
-    private void checkStoryProperties(Story story) throws CardioFunctionalException {
+		if (status != null) {
+			if (!story.getStatus().toString().equals(status)) {
+				updateTimestamp = true;
+			}
+			story.setStatus(StoryStatus.valueOf(status));
+		}
+		if (contribution != null) {
+			story.setContribution(Integer.parseInt(contribution));
+		}
+		if (estimate != null) {
+			story.setEstimate(Integer.parseInt(estimate));
+		}
+		if (assignedUser != null) {
+			story.setAssignedUser(Long.parseLong(assignedUser));
+		}
+		return update(story, updateTimestamp);
+	}
 
-        if (story == null) {
-            LOGGER.error("story can't be null");
-            throw new CardioFunctionalException("story can't be null");
-        }
-        if (story.getStatus() == null) {
-            LOGGER.error("status is mandatory");
-            throw new CardioFunctionalException("status is mandatory");
-        }
-        Fields.checkField("description", story.getDescription(), 0, 256);
-    }
+	private void checkStoryProperties(Story story) throws CardioFunctionalException {
 
-    @Override
-    public String export() throws CardioTechnicalException {
-        List<Story> stories = all();
-        StringBuffer sb = new StringBuffer();
+		if (story == null) {
+			LOGGER.error("story can't be null");
+			throw new CardioFunctionalException("story can't be null");
+		}
+		if (story.getStatus() == null) {
+			LOGGER.error("status is mandatory");
+			throw new CardioFunctionalException("status is mandatory");
+		}
+		Fields.checkField("description", story.getDescription(), 0, 256);
+	}
 
-        for (Story us : stories) {
-            if (us.getAssignedUser() == null) {
-                sb.append(String.format(SQL_EXPORT_WITHOUT_USER, escapeSpecialCharacters(us.getDescription()),
-                        us.getStatus().toString(), us.getLastUpdate(), us.getContribution(), us.getEstimate()));
-            } else {
-                sb.append(String.format(SQL_EXPORT, escapeSpecialCharacters(us.getDescription()),
-                        us.getStatus().toString(), us.getLastUpdate(), us.getContribution(), us.getEstimate(),
-                        us.getAssignedUser()));
-            }
-        }
+	@Override
+	public String export() throws CardioTechnicalException {
+		List<Story> stories = all();
+		StringBuffer sb = new StringBuffer();
 
-        return sb.toString();
-    }
+		for (Story us : stories) {
+			if (us.getAssignedUser() == null) {
+				sb.append(String.format(SQL_EXPORT_WITHOUT_USER, escapeSpecialCharacters(us.getDescription()), us.getStatus().toString(),
+						convertLocalDateTime(us.getLastUpdate()), us.getContribution(), us.getEstimate()));
+			} else {
+				sb.append(String.format(SQL_EXPORT, escapeSpecialCharacters(us.getDescription()), us.getStatus().toString(),
+						convertLocalDateTime(us.getLastUpdate()), us.getContribution(), us.getEstimate(), us.getAssignedUser()));
+			}
+		}
 
-    private String escapeSpecialCharacters(String txt) {
-        String escapedTxt = null;
-        if (txt != null) {
-            escapedTxt = txt.replaceAll("'", "''");
-        }
-        return escapedTxt;
-    }
+		return sb.toString();
+	}
+
+	private String escapeSpecialCharacters(String txt) {
+		String escapedTxt = null;
+		if (txt != null) {
+			escapedTxt = txt.replaceAll("'", "''");
+		}
+		return escapedTxt;
+	}
+
+	private String convertLocalDateTime(LocalDateTime dateTime) {
+		String stringDate = null;
+		if (dateTime != null) {
+			stringDate = DATE_EXPORT_FORMAT.print(dateTime);
+		}
+		return stringDate;
+	}
 }
--- cardio-common/src/test/java/com/sopra/agile/cardio/common/utils/LocalDateUtilsTest.java
+++ cardio-common/src/test/java/com/sopra/agile/cardio/common/utils/LocalDateUtilsTest.java
@@ -12,72 +12,66 @@ import org.junit.Test;
 
 public class LocalDateUtilsTest {
 
-    private static final long NOW = System.currentTimeMillis() / 1000 * 1000;
+	private static final long NOW = System.currentTimeMillis() / 1000 * 1000;
 
-    private static final String SHOULD_BE_EQUALS = "Should be equals";
+	private static final String SHOULD_BE_EQUALS = "Should be equals";
 
-    @Test
-    public void testConvertToLocalDate() {
-        assertEquals(SHOULD_BE_EQUALS, getLocalDateFromLong(NOW),
-                LocalDateUtils.convertToLocalDate(getDateFromLong(NOW)));
-        assertNull(LocalDateUtils.convertToLocalDate(null));
-    }
+	@Test
+	public void testConvertToLocalDate() {
+		assertEquals(SHOULD_BE_EQUALS, getLocalDateFromLong(NOW), LocalDateUtils.convertToLocalDate(getDateFromLong(NOW)));
+		assertNull(LocalDateUtils.convertToLocalDate(null));
+	}
 
-    @Test
-    public void convertLDToDate() {
-        assertEquals(SHOULD_BE_EQUALS, getExactDateFromLong(NOW),
-                LocalDateUtils.convertToDate(getLocalDateFromLong(NOW)));
-        assertNull(LocalDateUtils.convertToDate((LocalDate) null));
-    }
+	@Test
+	public void convertLDToDate() {
+		assertEquals(SHOULD_BE_EQUALS, getExactDateFromLong(NOW), LocalDateUtils.convertToDate(getLocalDateFromLong(NOW)));
+		assertNull(LocalDateUtils.convertToDate((LocalDate) null));
+	}
 
-    private Date getDateFromLong(long time) {
-        Date today = new Date(time);
-        return today;
-    }
+	private Date getDateFromLong(long time) {
+		Date today = new Date(time);
+		return today;
+	}
 
-    private Date getExactDateFromLong(long time) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(time);
-        cal.set(Calendar.HOUR_OF_DAY, 0);
-        cal.set(Calendar.MINUTE, 0);
-        cal.set(Calendar.SECOND, 0);
-        cal.set(Calendar.MILLISECOND, 0);
-        return new Date(cal.getTimeInMillis());
-    }
+	private Date getExactDateFromLong(long time) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(time);
+		cal.set(Calendar.HOUR_OF_DAY, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+		return new Date(cal.getTimeInMillis());
+	}
 
-    private LocalDate getLocalDateFromLong(long time) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(time);
-        return new LocalDate(
-                String.format("%s-%s-%s", cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DATE)));
-    }
+	private LocalDate getLocalDateFromLong(long time) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(time);
+		return new LocalDate(String.format("%s-%s-%s", cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DATE)));
+	}
 
-    @Test
-    public void testConvertToLocalDateTime() {
-        assertEquals(SHOULD_BE_EQUALS, getLocalDateTimeFromLong(NOW),
-                LocalDateUtils.convertToLocalDateTime(getDateFromLong(NOW)));
-        assertNull(LocalDateUtils.convertToLocalDateTime(null));
-    }
+	@Test
+	public void testConvertToLocalDateTime() {
+		assertEquals(SHOULD_BE_EQUALS, getLocalDateTimeFromLong(NOW), LocalDateUtils.convertToLocalDateTime(getDateFromLong(NOW)));
+		assertNull(LocalDateUtils.convertToLocalDateTime((Date) null));
+	}
 
-    @Test
-    public void convertLDTToDate() {
-        assertEquals(SHOULD_BE_EQUALS, getTimestampFromLong(NOW),
-                LocalDateUtils.convertToDate(getLocalDateTimeFromLong(NOW)));
-        assertNull(LocalDateUtils.convertToDate((LocalDateTime) null));
-    }
+	@Test
+	public void convertLDTToDate() {
+		assertEquals(SHOULD_BE_EQUALS, getTimestampFromLong(NOW), LocalDateUtils.convertToDate(getLocalDateTimeFromLong(NOW)));
+		assertNull(LocalDateUtils.convertToDate((LocalDateTime) null));
+	}
 
-    private Date getTimestampFromLong(long time) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(time);
-        cal.set(Calendar.MILLISECOND, 0);
-        return new Date(cal.getTimeInMillis());
-    }
+	private Date getTimestampFromLong(long time) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(time);
+		cal.set(Calendar.MILLISECOND, 0);
+		return new Date(cal.getTimeInMillis());
+	}
 
-    private LocalDateTime getLocalDateTimeFromLong(long time) {
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(time);
-        return new LocalDateTime(String.format("%s-%s-%sT%2d:%2d:%2d", cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DATE), cal.get(Calendar.HOUR_OF_DAY),
-                cal.get(Calendar.MINUTE), cal.get(Calendar.SECOND)));
-    }
+	private LocalDateTime getLocalDateTimeFromLong(long time) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(time);
+		return new LocalDateTime(String.format("%s-%s-%sT%02d:%02d:%02d", cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DATE),
+				cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), cal.get(Calendar.SECOND)));
+	}
 }
