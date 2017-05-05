be54e4fdfea1e71ccaf532ee5a96bd148f9017b1
stephdav <stephane.david@soprasteria.com>
2017-05-05 16:21:27 +0200

    Update story

--- cardio-ui/src/main/resources/public/js/story.js
+++ cardio-ui/src/main/resources/public/js/story.js
@@ -5,6 +5,16 @@ $(document).ready(function() {
 function initStory() {
 	var storyId = getLastPathLocation();
 	getStory(storyId);
+	
+	$('#updateStory').on('click', function(e) {
+		e.stopPropagation();
+		updateStory($('#storyId').text(), $('#storyDescription').val(), $('#storyStatus').val(), $('#storyEstimate').val());
+	});
+
+	$('#deleteStory').on('click', function(e) {
+		e.stopPropagation();
+		deleteStory($('#storyId').val());
+	});
 }
 
 function getStory(id) {
@@ -13,10 +23,26 @@ function getStory(id) {
 
 function callbackStory(story, hv) {
 	log("story: "+JSON.stringify(story));
-	$('#storyId').text('#' + story.id);
+	$('#storyId').text(story.id);
 	$('#storyDescription').val(story.description);
 	$('#storyStatus option').prop('selected', false);
 	$('#storyStatus option[value="' + story.status+ '"]').prop('selected', true);
 	$('#storyEstimate option').prop('selected', false);
 	$('#storyEstimate option[value="' + story.estimate+ '"]').prop('selected', true);
+}
+
+function updateStory(id, description, status, estimate) {
+	var payload = {id: id, description: description, status: status, estimate: estimate};
+	ajaxPost("/api/stories/"+id, payload, function(data, hv, errorThrown) {
+		if (hv.status == 201 || hv.status == 200) {
+			log("Story " + hv.location + " updated");
+		} else {
+			log("Error updating story : " + errorThrown);
+			$('#errors').text(data);
+			$('.form-error').show();
+		}
+	});
+}
+
+function deleteStory(id) {
 }
