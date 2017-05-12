85b6b872c694f1425b157a25b41c0ffdbc1c5ce2
stephdav <stephane.david@soprasteria.com>
2017-05-12 19:08:31 +0200

    windows package

--- cardio-dist/src/main/scripts/startCardio.bat
+++ cardio-dist/src/main/scripts/startCardio.bat
@@ -1,4 +1,4 @@
 cd %~dp0
 
 echo Starting cardio...
-start "cardioApp" java -classpath ".;lib/*" com.sopra.agile.cardio.app.App
+start "cardioApp" java -classpath "lib/*" com.sopra.agile.cardio.app.App
